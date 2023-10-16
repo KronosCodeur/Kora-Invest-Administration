8b39b369a0d3013b44db7da240fbb35568971f5c
Kronos Codeur <piraliarris@gmail.com>
2023-10-16 00:44:03 +0000

    home UI building

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
+    /**
+     * @param UserRepository $userRepository
+     */
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
 
     #[Route("/",name: "welcome",methods: ['GET'])]
     public function index(): Response
@@ -17,6 +26,18 @@ class HomeController extends AbstractController
     #[Route("/admin",name: "home.index",methods: ['GET'])]
     public function home(): Response
     {
-        return $this->render('home/index.html.twig');
+        $users = $this->userRepository->findBy(['roles'=>"ROLE_ADMIN"]);
+        $count =0;
+        foreach ($users as $user) {
+            $roles[] = $user->getRoles;
+            if ($roles)
+            $count++;
+        }
+        return $this->render('home/index.html.twig',[
+            "users"=>$count
+        ]);
     }
+
+    private UserRepository $userRepository;
+
 }
