3ffe29db4fadd29a0bff28c9561d5eb97d0a501f
Kronos Codeur <piraliarris@gmail.com>
2023-10-16 11:22:41 +0000

    ui corrections : Home page

--- migrations/Version20231016103950.php
+++ migrations/Version20231016103950.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231014195208 extends AbstractMigration
+final class Version20231016103950 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,11 +23,21 @@ final class Version20231014195208 extends AbstractMigration
         $this->addSql('CREATE TABLE account (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, owner_id INTEGER NOT NULL, type VARCHAR(255) NOT NULL, number VARCHAR(255) NOT NULL, active BOOLEAN NOT NULL, created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
         , solde DOUBLE PRECISION NOT NULL, CONSTRAINT FK_7D3656A47E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_7D3656A47E3C61F9 ON account (owner_id)');
+        $this->addSql('CREATE TABLE city (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, country_id INTEGER NOT NULL, name VARCHAR(255) NOT NULL, CONSTRAINT FK_2D5B0234F92F3E70 FOREIGN KEY (country_id) REFERENCES country (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE INDEX IDX_2D5B0234F92F3E70 ON city (country_id)');
+        $this->addSql('CREATE TABLE contribution (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, contributor_id INTEGER NOT NULL, amount INTEGER NOT NULL, maked_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , CONSTRAINT FK_EA351E157A19A357 FOREIGN KEY (contributor_id) REFERENCES user (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE INDEX IDX_EA351E157A19A357 ON contribution (contributor_id)');
+        $this->addSql('CREATE TABLE country (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, flag VARCHAR(255) NOT NULL, code VARCHAR(255) NOT NULL, currency VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE TABLE fees (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, rate DOUBLE PRECISION NOT NULL)');
+        $this->addSql('CREATE TABLE investment (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, number VARCHAR(255) NOT NULL, solde INTEGER NOT NULL, blocked BOOLEAN NOT NULL, type VARCHAR(255) NOT NULL, maked_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , available_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , return INTEGER NOT NULL)');
         $this->addSql('CREATE TABLE "transaction" (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, account_id INTEGER NOT NULL, type VARCHAR(255) NOT NULL, amount INTEGER NOT NULL, fees DOUBLE PRECISION NOT NULL, maked_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
         , CONSTRAINT FK_723705D19B6B5FBA FOREIGN KEY (account_id) REFERENCES account (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_723705D19B6B5FBA ON "transaction" (account_id)');
         $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
-        , password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, gender VARCHAR(255) NOT NULL, picture VARCHAR(255) NOT NULL, active BOOLEAN NOT NULL, birthday DATETIME NOT NULL, address VARCHAR(255) NOT NULL)');
+        , password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, gender VARCHAR(255) NOT NULL, picture VARCHAR(255) NOT NULL, active BOOLEAN NOT NULL, birthday VARCHAR(10) NOT NULL, address VARCHAR(255) NOT NULL)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON user (username)');
         $this->addSql('CREATE TABLE messenger_messages (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, body CLOB NOT NULL, headers CLOB NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL)');
         $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
@@ -39,6 +49,11 @@ final class Version20231014195208 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE account');
+        $this->addSql('DROP TABLE city');
+        $this->addSql('DROP TABLE contribution');
+        $this->addSql('DROP TABLE country');
+        $this->addSql('DROP TABLE fees');
+        $this->addSql('DROP TABLE investment');
         $this->addSql('DROP TABLE "transaction"');
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE messenger_messages');
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ContributionRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +11,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
 
+
     /**
      * @param UserRepository $userRepository
+     * @param ContributionRepository $contributionRepository
      */
-    public function __construct(UserRepository $userRepository)
+    public function __construct(UserRepository $userRepository, ContributionRepository $contributionRepository)
     {
         $this->userRepository = $userRepository;
+        $this->contributionRepository = $contributionRepository;
     }
 
     #[Route("/",name: "welcome",methods: ['GET'])]
@@ -26,18 +30,22 @@ class HomeController extends AbstractController
     #[Route("/admin",name: "home.index",methods: ['GET'])]
     public function home(): Response
     {
-        $users = $this->userRepository->findBy(['roles'=>"ROLE_ADMIN"]);
+        $users = $this->userRepository->findAll();
+        $contributions = $this->contributionRepository->findAll();
         $count =0;
         foreach ($users as $user) {
-            $roles[] = $user->getRoles;
-            if ($roles)
+
+            if ($user->getRoles()===["ROLE_ADMIN"])
             $count++;
         }
         return $this->render('home/index.html.twig',[
-            "users"=>$count
+            "users"=>$users,
+            "clients"=>$count,
+            "contributions"=>$contributions
         ]);
     }
 
     private UserRepository $userRepository;
+    private  ContributionRepository  $contributionRepository;
 
 }
