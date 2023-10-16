de705969bc514fa515ff4c25ee32a1c2b7c3b873
Kronos Codeur <piraliarris@gmail.com>
2023-10-16 19:33:15 +0000

    clients ui and register process building

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ClientController extends AbstractController
 {
+    /**
+     * @param UserRepository $userRepository
+     */
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     #[Route('/admin/clients', name: 'clients.index')]
     public function index(): Response
     {
+        $clients = $this->userRepository->findAll();
         return $this->render('client/index.html.twig', [
-            'controller_name' => 'ClientController',
+            'clients' => $clients,
         ]);
     }
+    private UserRepository $userRepository;
+
 }
