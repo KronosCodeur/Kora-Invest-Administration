2bdd4b9e016e420dfcf2fad0fac825f0513481a5
Kronos Codeur <piraliarris@gmail.com>
2023-10-20 18:22:49 +0000

    ui perform

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\AccountRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountController extends AbstractController
 {
-    #[Route('/account', name: 'app_account')]
+    /**
+     * @param AccountRepository $accountRepository
+     */
+    public function __construct(AccountRepository $accountRepository)
+    {
+        $this->accountRepository = $accountRepository;
+    }
+
+    #[Route('/admin/account', name: 'account.index')]
     public function index(): Response
     {
+        $accounts = $this->accountRepository->findAll();
         return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
+            'accounts' => $accounts,
         ]);
     }
+    private  AccountRepository $accountRepository;
 }
