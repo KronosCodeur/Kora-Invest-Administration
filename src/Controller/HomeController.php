1cf4eab55ff6cea13dff06337076a4f9053cf994
Kronos Codeur <piraliarris@gmail.com>
2023-10-20 18:21:42 +0000

    home stats process perform

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use App\Repository\ContributionRepository;
 use App\Repository\IncomeRepository;
 use App\Repository\InvestmentRepository;
 use App\Repository\UserRepository;
+use App\Utils\DateTimeUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,13 +20,15 @@ class HomeController extends AbstractController
      * @param ContributionRepository $contributionRepository
      * @param IncomeRepository $incomeRepository
      * @param InvestmentRepository $investmentRepository
+     * @param DateTimeUtils $dateTimeUtils
      */
-    public function __construct(UserRepository $userRepository, ContributionRepository $contributionRepository, IncomeRepository $incomeRepository, InvestmentRepository $investmentRepository)
+    public function __construct(UserRepository $userRepository, ContributionRepository $contributionRepository, IncomeRepository $incomeRepository, InvestmentRepository $investmentRepository, DateTimeUtils $dateTimeUtils)
     {
         $this->userRepository = $userRepository;
         $this->contributionRepository = $contributionRepository;
         $this->incomeRepository = $incomeRepository;
         $this->investmentRepository = $investmentRepository;
+        $this->dateTimeUtils = $dateTimeUtils;
     }
 
     #[Route("/",name: "welcome",methods: ['GET'])]
@@ -38,23 +41,37 @@ class HomeController extends AbstractController
     {
         $users = $this->userRepository->findAll();
         $contributions = $this->contributionRepository->findAll();
+        $dailyContributions =0;
+        foreach ($contributions as $contribution) {
+            if($this->dateTimeUtils->dateIsNow($contribution->getMakedAt(),format: 'Y-m-d')){
+                $dailyContributions++;
+            }
+        }
         $investments = $this->investmentRepository->findBy(['blocked'=>true]);
+        $dailyInvestment = 0;
+        foreach ($investments as $investment) {
+            if($this->dateTimeUtils->dateIsNow($investment->getMakedAt(),format: 'Y-m-d')){
+                $dailyInvestment++;
+            }
+        }
         $dailyIncomesAmount = 0;
         $incomes = $this->incomeRepository->findAll();
         foreach ($incomes as $income) {
-            $dailyIncomesAmount += $income->getAmount();
+            if($this->dateTimeUtils->dateIsNow(substr($income->getMakedAt(),0,10),"Y-m-d")){
+                $dailyIncomesAmount += $income->getAmount();
+            }
         }
         $count =0;
         foreach ($users as $user) {
-            if ($user->getRoles()===["ROLE_CLIENT"])
+            if ($user->getRoles()===["ROLE_CLIENT"] && $this->dateTimeUtils->dateIsNow($user->getRegisteredAt(),format: 'Y-m-d') )
             $count++;
         }
         return $this->render('home/index.html.twig',[
             "users"=>$users,
             "clients"=>$count,
             "dailyIncomes"=>$dailyIncomesAmount,
-            "contributions"=>$contributions,
-            "investments"=>$investments
+            "contributions"=>$dailyContributions,
+            "investments"=>$dailyInvestment
         ]);
     }
 
@@ -62,5 +79,6 @@ class HomeController extends AbstractController
     private  ContributionRepository  $contributionRepository;
     private  IncomeRepository $incomeRepository;
     private  InvestmentRepository $investmentRepository;
+    private  DateTimeUtils $dateTimeUtils;
 
 }
