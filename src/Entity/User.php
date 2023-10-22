0d04692c6aa97228c7b2e4715613e76c3bf0aa30
Kronos Codeur <piraliarris@gmail.com>
2023-10-22 17:41:24 +0000

    code refactoring

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -399,4 +399,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->firstName." ".$this->lastName;
+    }
 }
