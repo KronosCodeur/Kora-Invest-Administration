f7cf4c60d429857583e76bc8d22e506a65c47fb5
Kronos Codeur <piraliarris@gmail.com>
2023-10-22 18:20:21 +0000

    contributions module

--- src/Controller/ContributionController.php
+++ src/Controller/ContributionController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\ContributionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContributionController extends AbstractController
 {
-    #[Route('/contribution', name: 'app_contribution')]
+
+    /**
+     * @param ContributionRepository $contributionRepository
+     */
+    public function __construct(ContributionRepository $contributionRepository)
+    {
+        $this->contributionRepository = $contributionRepository;
+    }
+
+    #[Route('/admin/contributions', name: 'contribution.index')]
     public function index(): Response
     {
+        $contributions = array_reverse($this->contributionRepository->findAll());
         return $this->render('contribution/index.html.twig', [
-            'controller_name' => 'ContributionController',
+            'contributions' =>$contributions
         ]);
     }
+    private  ContributionRepository $contributionRepository;
 }
