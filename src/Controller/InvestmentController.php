455ee29e756c0590e94be3866dca89eda93cfba0
Kronos Codeur <piraliarris@gmail.com>
2023-10-24 00:01:21 +0000

    investments module: logic - routing and process

--- src/Controller/InvestmentController.php
+++ src/Controller/InvestmentController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\InvestmentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class InvestmentController extends AbstractController
 {
-    #[Route('/admin/investment', name: 'investment.index')]
+    /**
+     * @param InvestmentRepository $investmentRepository
+     */
+    public function __construct(InvestmentRepository $investmentRepository)
+    {
+        $this->investmentRepository = $investmentRepository;
+    }
+
+    #[Route('/admin/investments', name: 'investment.index')]
     public function index(): Response
     {
+        $investments = array_reverse($this->investmentRepository->findAll());
         return $this->render('investment/index.html.twig', [
-            'controller_name' => 'InvestmentController',
+            'investments' =>$investments
         ]);
     }
+    private  InvestmentRepository $investmentRepository;
 }
